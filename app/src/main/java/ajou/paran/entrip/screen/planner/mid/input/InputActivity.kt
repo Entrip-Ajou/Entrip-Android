3e16feb8a9d6517b5f6926849f71397f4e0f4f19
sangjin-hash <lsj9873@ajou.ac.kr>
2022-03-13 19:27:37 +0900

    Minute Issue Completed

--- app/src/main/java/ajou/paran/entrip/screen/planner/mid/input/InputActivity.kt
+++ app/src/main/java/ajou/paran/entrip/screen/planner/mid/input/InputActivity.kt
@@ -14,13 +14,14 @@ import java.util.*
 
 @AndroidEntryPoint
 class InputActivity : BaseActivity<ActivityInputBinding>(
-    R.layout.activity_input) {
+    R.layout.activity_input
+) {
 
-    companion object{
+    companion object {
         private const val TAG = "[InputActivity]"
     }
 
-    private val viewModel : InputViewModel by viewModels()
+    private val viewModel: InputViewModel by viewModels()
 
     override fun init() {
         binding.inputViewModel = viewModel
@@ -28,8 +29,8 @@ class InputActivity : BaseActivity<ActivityInputBinding>(
     }
 
     private fun setUpObserver() {
-        viewModel.inputState.observe(this){
-            when(it){
+        viewModel.inputState.observe(this) {
+            when (it) {
                 is InputState.initialized -> {
                     binding.todoStar.visibility = View.VISIBLE
                     binding.todoNotNull.visibility = View.VISIBLE
@@ -59,8 +60,8 @@ class InputActivity : BaseActivity<ActivityInputBinding>(
     }
 
     // backButton, time, location 클릭 이벤트 처리
-    fun onClick(v:View){
-        when(v.id){
+    fun onClick(v: View) {
+        when (v.id) {
             binding.backButton.id -> {
 
             }
@@ -70,9 +71,16 @@ class InputActivity : BaseActivity<ActivityInputBinding>(
                 val startHour = currentTime.get(Calendar.HOUR_OF_DAY)
                 val startMinute = currentTime.get(Calendar.MINUTE)
 
-                TimePickerDialog(this, TimePickerDialog.OnTimeSetListener{
-                    view, hourOfDay, minute -> binding.tvTime.setText("$hourOfDay : $minute")},
-                    startHour, startMinute, true).show()
+                TimePickerDialog(
+                    this, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        if (minute < 10) {
+                            binding.tvTime.setText("$hourOfDay : 0$minute")
+                        } else {
+                            binding.tvTime.setText("$hourOfDay : $minute")
+                        }
+                    },
+                    startHour, startMinute, true
+                ).show()
             }
 
             binding.tvLocation.id -> {
@@ -82,38 +90,73 @@ class InputActivity : BaseActivity<ActivityInputBinding>(
     }
 
     // 색상 선택
-    fun selectPalette(v:View){
-        when(v.id){
+    fun selectPalette(v: View) {
+        when (v.id) {
             binding.color.id -> {
                 binding.etTodo.setBackgroundColor(Color.WHITE)
                 viewModel.rgb.value = Color.WHITE
             }
             binding.color1.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.red))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.red
+                    )
+                )
                 viewModel.rgb.value = R.color.red
             }
             binding.color2.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.orange))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.orange
+                    )
+                )
                 viewModel.rgb.value = R.color.orange
             }
             binding.color3.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.yellow))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.yellow
+                    )
+                )
                 viewModel.rgb.value = R.color.yellow
             }
             binding.color4.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.green))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.green
+                    )
+                )
                 viewModel.rgb.value = R.color.green
             }
             binding.color5.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.blue))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.blue
+                    )
+                )
                 viewModel.rgb.value = R.color.blue
             }
             binding.color6.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.indigo))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.indigo
+                    )
+                )
                 viewModel.rgb.value = R.color.indigo
             }
             binding.color7.id -> {
-                binding.etTodo.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.purple))
+                binding.etTodo.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.purple
+                    )
+                )
                 viewModel.rgb.value = R.color.purple
             }
         }
